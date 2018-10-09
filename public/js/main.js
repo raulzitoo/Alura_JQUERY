7be245bfe86ff71d872a0486d5c418aa45ed3b7b
raulzitoo <raulzito.j@gmail.com>
2018-10-09 11:45:43 -0700

    Capitulo 4 Reiniciando o Jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,53 @@
-var frase = $(".frase").text();
-var palavras = frase.split(" ").length;
-$("#tamanho-frase").text(palavras);
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input",function(){
-    console.log(campo.val());
-    $("#contador-caracteres").text(campo.val().length);
-    $("#contador-palavras").text(campo.val().split(/\S+/).length -1);
+
+$(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
 
-campo.one("focus",function(){
+$("#botao-reiniciar").click(reiniciaJogo);
+
+
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text();
+    var palavras = frase.split(" ").length;
+    $("#tamanho-frase").text(palavras);
+}
+
+function inicializaContadores(){
+    campo.on("input",function(){
+        console.log(campo.val());
+        $("#contador-caracteres").text(campo.val().length);
+        $("#contador-palavras").text(campo.val().split(/\S+/).length -1);
+    });
+}
+
+function inicializaCronometro(){
     
-    var cronometroId = setInterval(function(){
-            tempoRestante --;
-            $("#tempo-digitacao").text(tempoRestante);
-            if (tempoRestante < 1){
-                campo.attr("disabled",true);
-                clearInterval(cronometroId);
-            }else{
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus",function(){ 
+        var cronometroId = setInterval(function(){
+                tempoRestante --;
+                $("#tempo-digitacao").text(tempoRestante);
+                if (tempoRestante < 1){
+                    campo.attr("disabled",true);
+                    clearInterval(cronometroId);
+                }else{
+                        
+                }        
+            },1000);    
+    });
+}
 
-            }        
-        },1000);    
-});
+
+function reiniciaJogo(){
+    campo.attr("disabled",false);
+    campo.val("");
+    $("#contador-caracteres").text("0");
+    $("#contador-palavras").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
