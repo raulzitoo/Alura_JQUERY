7f1b6c6a52e36469be29b8442b5f0a46b58739c2
raulzitoo <raulzito.j@gmail.com>
2018-10-08 17:48:09 -0700

    Capitulo 3 Game Over Com Eventos

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,19 @@ campo.on("input",function(){
     $("#contador-caracteres").text(campo.val().length);
     $("#contador-palavras").text(campo.val().split(/\S+/).length -1);
 });
+
+var tempoRestante = $("#tempo-digitacao").text();
+
+campo.one("focus",function(){
+    
+    var cronometroId = setInterval(function(){
+            tempoRestante --;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1){
+                campo.attr("disabled",true);
+                clearInterval(cronometroId);
+            }else{
+
+            }        
+        },1000);    
+});
