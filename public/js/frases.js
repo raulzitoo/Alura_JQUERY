9f30292f02bfff9f86d9d978be98be6ffaf6ec1a
raulzitoo <raulzito.j@gmail.com>
2018-10-23 17:43:01 -0700

    JQUERY 2 - CAPITULO 4 - Enviando dados com AJAX

--- public/js/frases.js
+++ public/js/frases.js
@@ -3,12 +3,7 @@ $("#botao-frase").click(fraseAleatoria);
 function fraseAleatoria(){
     $("#spinner").show();
     $.get("http://localhost:3000/frases",trocaFraseAleatoria)
-    .fail(function(){
-            $("#erro").show();
-            setTimeout(function(){
-                $("#erro").toggle();
-            },1500);
-        })
+    .fail(falha)
     .always(function(){
                 $("#spinner").toggle();
             });    
@@ -29,4 +24,36 @@ function trocaFraseAleatoria(data){
 function atualizaTempoInicial(tempo){
     tempoInicial = tempo;
     $("#tempo-digitacao").text(tempo);
+}
+
+$("#botao-frase-id").click(buscaFrase);
+
+function buscaFrase(){
+    
+    $("#spinner").show();
+    var fraseId = $("#frase-id").val();
+    var dados = {id : fraseId};
+
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+     .fail(falha)
+     .always(function(){
+        $("#spinner").toggle();
+     });
+};
+
+function falha(){
+    $("#erro").show();
+    setTimeout(function(){
+        $("#erro").toggle();
+    },1500);
+}
+
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    console.log(data);
+    frase.text(data.texto);
+    fraseInicial = data.texto;
+    atualizaTamanhoFrase(data.texto);
+    atualizaTempoInicial(data.tempo);
 }
